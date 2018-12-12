39d7369c2dc78e9172f8ada93806fbf8b9947ec0
Klaus Schneider-Zapp <klaus_snd@web.de>
2018-12-12 19:49:07 +0100

    Add FIFO and tests

---
+++ include/utils/fifo.h
@@ -0,0 +1,110 @@
+/*  Hardware abstraction layer for embedded systems
+ *
+ *  (c) Klaus Schneider-Zapp klaus underscore snd at web dot de
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ */
+
+#pragma once
+
+#include <stddef.h>
+
+/** FIFO
+ *
+ *  @note The size of the ring buffer _must_ be a power of 2!
+ */
+template <typename T, size_t buffer_size, typename S = uint8_t>
+class Fifo
+{
+public:
+   using size_type = S;
+   using value_type = T;
+
+   Fifo();
+
+   bool isEmpty() const;
+   bool isFull() const;
+   /// Return the number of entries in the FIFO
+   size_type count() const;
+   constexpr size_type capacity() const
+   { return static_cast<size_type>(buffer_size); }
+   /// Write a value into the FIFO.
+   /// @return false if the FIFO is full and the value could not be added
+   bool write(T value);
+   /// Read a value from the FIFO. If the FIFO is empty, returns zero.
+   T read();
+
+private:
+   /// new value of the write index when inserting @e itemCount values
+   size_type writeIndexWhenAddingItems(size_type itemCount) const
+   { return (m_writeIndex + itemCount) & (capacity() - 1); }
+
+   size_type m_readIndex;
+   size_type m_writeIndex;
+   T m_buffer[buffer_size];
+};
+
+template <typename T, size_t buffer_size, typename S>
+Fifo<T, buffer_size, S>::Fifo()
+      : m_readIndex(0)
+      , m_writeIndex(0)
+{
+   static_assert(buffer_size % 2 == 0, "Buffer size must be a multiple of 2");
+}
+
+template <typename T, size_t buffer_size, typename S>
+bool Fifo<T, buffer_size, S>::isEmpty() const
+{
+   return m_readIndex == m_writeIndex;
+}
+
+template <typename T, size_t buffer_size, typename S>
+bool Fifo<T, buffer_size, S>::isFull() const
+{
+   const auto newWriteIndex = writeIndexWhenAddingItems(1);
+   return newWriteIndex == m_readIndex;
+}
+
+template <typename T, size_t buffer_size, typename S>
+typename Fifo<T, buffer_size, S>::size_type Fifo<T, buffer_size, S>::count() const
+{
+   return (m_writeIndex - m_readIndex) & (capacity() - 1);
+}
+
+template <typename T, size_t buffer_size, typename S>
+bool Fifo<T, buffer_size, S>::write(T value)
+{
+   const auto newWriteIndex = writeIndexWhenAddingItems(1);
+   const bool isFull = newWriteIndex == m_readIndex;
+   if (isFull)
+   {
+      return false;
+   }
+   else
+   {
+      m_writeIndex = newWriteIndex;
+      m_buffer[newWriteIndex] = value;
+      return true;
+   }
+}
+
+template <typename T, size_t buffer_size, typename S>
+T Fifo<T, buffer_size, S>::read()
+{
+   if (isEmpty())
+   {
+      return {};
+   }
+   else
+   {
+      m_readIndex = (m_readIndex + 1) & (capacity() - 1);
+      return m_buffer[m_readIndex];
+   }
+}
---
+++ tests/test_fifo.cpp
@@ -0,0 +1,110 @@
+#include <gmock/gmock.h>
+
+#include <utils/fifo.h>
+
+using namespace testing;
+
+class AFifo : public testing::Test
+{
+public:
+   static const uint8_t BUFFER_SIZE = 8;
+   static const uint8_t ARBITRARY_VALUE = 10;
+
+   Fifo<uint8_t, BUFFER_SIZE> fifo;
+};
+
+TEST_F(AFifo, isEmptyOnConstruction)
+{
+   ASSERT_TRUE(fifo.isEmpty());
+   ASSERT_FALSE(fifo.isFull());
+   ASSERT_THAT(fifo.count(), Eq(0));
+}
+
+TEST_F(AFifo, readingFromEmptyObtainsZero)
+{
+   ASSERT_THAT(fifo.read(), Eq(0));
+}
+
+TEST_F(AFifo, isNonEmptyAfterAddingOneElement)
+{
+   fifo.write(ARBITRARY_VALUE);
+
+   ASSERT_FALSE(fifo.isEmpty());
+   ASSERT_THAT(fifo.count(), Eq(1));
+}
+
+TEST_F(AFifo, writeAndReadObtainsOriginalValue)
+{
+   fifo.write(ARBITRARY_VALUE);
+
+   ASSERT_THAT(fifo.read(), Eq(ARBITRARY_VALUE));
+}
+
+TEST_F(AFifo, readingAfterWritingMultipleValuesObtainsFirstValue)
+{
+   fifo.write(0);
+   fifo.write(1);
+
+   ASSERT_THAT(fifo.read(), Eq(0));
+}
+
+TEST_F(AFifo, readingTwiceObtainsSecondValue)
+{
+   fifo.write(0);
+   fifo.write(1);
+   fifo.read();
+
+   ASSERT_THAT(fifo.read(), Eq(1));
+}
+
+TEST_F(AFifo, addingBufferSizeMinusOneValuesMakesItFull)
+{
+   for (uint8_t i = 0; i < BUFFER_SIZE - 1; ++i)
+   {
+      ASSERT_TRUE(fifo.write(i));
+   }
+
+   ASSERT_TRUE(fifo.isFull());
+}
+
+TEST_F(AFifo, tryingToWriteToFullReturnsFalse)
+{
+   for (uint8_t i = 0; i < BUFFER_SIZE - 1; ++i)
+   {
+      ASSERT_TRUE(fifo.write(i));
+   }
+
+   ASSERT_FALSE(fifo.write(ARBITRARY_VALUE));
+}
+
+class AFilledAndEmptiedFifo : public AFifo
+{
+   void SetUp() override
+   {
+      for (uint8_t i = 0; i < BUFFER_SIZE - 2; ++i)
+      {
+         fifo.write(i);
+      }
+      while (!fifo.isEmpty())
+      {
+         fifo.read();
+      }
+   }
+};
+
+TEST_F(AFilledAndEmptiedFifo, readObtainsFirstValue)
+{
+   fifo.write(33);
+   fifo.write(34);
+
+   ASSERT_THAT(fifo.read(), Eq(33));
+}
+
+TEST_F(AFilledAndEmptiedFifo, readingTwiceObtainsSecondValue)
+{
+   fifo.write(33);
+   fifo.write(34);
+   fifo.read();
+
+   ASSERT_THAT(fifo.read(), Eq(34));
+}
