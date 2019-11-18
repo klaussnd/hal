ad2be0f8f6d97c755dec659cbc2f6c50ae28706c
Klaus Schneider-Zapp <klaus_snd@web.de>
2019-11-18 18:21:54 +0100

    Add interface for i2c master

---
+++ include/hal/i2c_master.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <inttypes.h>
+
+enum class I2cStatus
+{
+   SUCCESS,
+   START_ERROR,
+   TRANSMIT_ERROR,
+   ACK_ERROR,
+};
+
+/** Initialise i2c interface */
+bool i2cMasterInit();
+
+/** Send data to i2c
+ */
+I2cStatus i2cMasterWrite(uint8_t dev_addr, const uint8_t* data, uint8_t length);
+
+/** Receive data from i2c.
+ *  Assert to read exactly \e length bytes.
+ */
+I2cStatus i2cMasterRead(uint8_t dev_addr, uint8_t* data, uint8_t length);
+
+/** Write data, send a restart condition and receive data.
+ */
+I2cStatus i2cMasterWriteThenRead(uint8_t dev_addr, const uint8_t* write_data,
+                                 uint8_t write_length, uint8_t* read_data,
+                                 uint8_t read_length);
