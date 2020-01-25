6f960957ec8bd478a128cc9603a9040006eb5ce7
Klaus Schneider-Zapp <klaus_snd@web.de>
2020-01-25 13:48:42 +0100

    Make file header consistent

--- include/hal/input/button.h
+++ include/hal/input/button.h
@@ -1,16 +1,16 @@
 /*  Debouncing max. 8 buttons, sampling 4 times, with repeat function
  *
  *  Original author: Peter Dannegger <danni@specs.de>
- *  Adjusted by Klaus Schneider-Zapp
+ *  Adjusted by Klaus Schneider-Zapp klaus underscore snd at web dot de
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 3
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3
  *  as published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Affero General Public License for more details.
  */
 
 #pragma once
--- include/hal/sensor/hyt939.h
+++ include/hal/sensor/hyt939.h
@@ -1,4 +1,18 @@
-// Hygrosens Hyt939 temperature and humidity sensor
+/*  Hardware abstraction layer for embedded systems
+ *  Hygrosens Hyt939 temperature and humidity sensor
+ *
+ *  (c) Klaus Schneider-Zapp klaus underscore snd at web dot de
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License version 3
+ *  as published by the Free Software Foundation.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ */
+
 #pragma once
 
 #if defined __GNUC__ && __GNUC__ < 7
