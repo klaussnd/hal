348074b9a127fe99d67121f53ae9fbbdf4b8c17e
Klaus Schneider-Zapp <klaus_snd@web.de>
2024-04-11 20:19:20 +0200

    Detach sensor communication from value computation

--- src/hal/sensor/hyt939.cpp
+++ src/hal/sensor/hyt939.cpp
@@ -7,7 +7,7 @@ namespace
 constexpr uint8_t HYT939_ADDR = 0x28;
 }
 
-std::optional<Hyt939Data> hyt939Measure()
+std::optional<Hyt939RawData> hyt939MeasureRaw()
 {
    uint8_t buffer[4] = {0};
 
@@ -32,14 +32,29 @@ std::optional<Hyt939Data> hyt939Measure()
       return {};
    }
 
-   Hyt939Data data;
    const uint16_t humi_raw = ((buffer[0] << 8) | buffer[1]) & 0x3fff;
    const uint16_t temp_raw = ((buffer[2] << 6) | (buffer[3] >> 2)) & 0x3fff;
-   data.humidity =
-      (static_cast<uint32_t>(humi_raw) * 100UL) / (static_cast<uint32_t>(2 << 13) - 1UL);
-   const uint16_t temp_tmp =
-      static_cast<uint32_t>(temp_raw) * 1650UL / (static_cast<uint32_t>(2 << 13) - 1UL);
+   return Hyt939RawData{humi_raw, temp_raw};
+}
+
+Hyt939Data hyt939Compute(const Hyt939RawData& raw)
+{
+   Hyt939Data data;
+   data.humidity = (static_cast<uint32_t>(raw.humidity) * 100UL)
+                   / (static_cast<uint32_t>(2 << 13) - 1UL);
+   const uint16_t temp_tmp = static_cast<uint32_t>(raw.temperature) * 1650UL
+                             / (static_cast<uint32_t>(2 << 13) - 1UL);
    data.temperature = static_cast<int16_t>(temp_tmp) - 400;
 
    return data;
+}
+
+std::optional<Hyt939Data> hyt939Measure()
+{
+   const auto raw = hyt939MeasureRaw();
+   if (!raw.has_value())
+   {
+      return {};
+   }
+   return hyt939Compute(raw.value());
 }
